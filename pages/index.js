629c24eec65b5a0c51ecd9ea6e46b114a6c975d3
okni-c <oknistay@gmail.com>
2022-05-03 23:30:01 -0500

    Added tings

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,28 @@
+import React, { useState } from "react";
 import Head from 'next/head'
 import Header from '@components/header/Header'
 import Footer from '@components/footer/Footer'
 import Typewriter from 'typewriter-effect';
 
 export default function Home() {
+
+  const [close, setClose] = useState();
+  const [open, setOpen] = useState({ opacity: '0' });
+  const [background, setBackground] = useState();
+  const [load, setLoad] = useState('main-page');
+
+  const cmdClose = () => {
+    setClose({ opacity: '0' });
+    console.log('CLosing!');
+  };
+
+  const pageOpen = () => {
+    setLoad('main-page scale-up-ver-top');
+    setOpen({ opacity: '1' });
+    setBackground({ backgroundColor: '#FFF' })
+    console.log('Opening content!');
+  };
+
   return (
     <div className="container">
       <Head>
@@ -14,18 +33,45 @@ export default function Home() {
       <main>
 
         <div className="container">
-          <img src="/Background.png" className="macintosh" />
+          <img src="/Background2.png" className="macintosh" />
           <img src="/scanlines.png" className="scanlines" />
           <div className="scanline"></div>
-          <div className="screen">
-            <div className="screen-content">
-              <Typewriter
-                options={{
-                  strings: ['Hello world.', 'Welcome to my little computer.'],
-                  autoStart: true,
-                  deleteSpeed: '100'
-                }}
-              />
+          <div className="screen" style={background}>
+            <div className="screen-content" style={close}>
+              <Typewriter onInit={(typewriter) => {
+                typewriter.start()
+                  .pauseFor(1000)
+                  .changeDelay(1)
+                  .typeString('Booting os 34.1...')
+                  .pauseFor(2500)
+                  .typeString('<br>Loading dom...')
+                  .pauseFor(1000)
+                  .typeString('<br>Loading assets...')
+                  .pauseFor(1600)
+                  .typeString('<br>Loading styles...')
+                  .pauseFor(1000)
+                  .typeString('<br>Loading bugs...')
+                  .pauseFor(0)
+                  .deleteAll(3)
+                  .typeString('starting...')
+                  .pauseFor(500)
+                  .callFunction(cmdClose)
+                  .pauseFor(500)
+                  .callFunction(pageOpen);
+              }} />
+            </div>
+            <div className={load} style={open}>
+              <div className="page-header">
+                <h1>Welcome to my site.</h1>
+                <img src="/spinearth.gif" className="globe" />
+              </div>
+              
+              <div className="links">
+                <a className="btn" href="/">Projects</a>
+                <a className="btn" href="/">Jobs</a>
+                <a className="btn" href="/">About</a>
+                <a className="btn" href="/">Contact</a>
+              </div>
             </div>
           </div>
         </div>
